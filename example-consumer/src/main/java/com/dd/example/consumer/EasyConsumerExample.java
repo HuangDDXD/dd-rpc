ccd2697f0b431bed67a2fd75659e2c7e5c228761
HuangDDXD <149939518+HuangDDXD@users.noreply.github.com>
2024-11-04 20:44:26 +0800

    day 01-简易版的rpc框架

---
+++ example-consumer/src/main/java/com/dd/example/consumer/EasyConsumerExample.java
@@ -0,0 +1,26 @@
+package com.dd.example.consumer;
+
+
+import com.dd.ddrpc.proxy.ServiceProxyFactory;
+import com.dd.example.common.model.User;
+import com.dd.example.common.service.UserService;
+
+/**
+ * 简易服务消费者示例
+ */
+public class EasyConsumerExample {
+
+    public static void main(String[] args) {
+        // 动态代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+        user.setName("dd");
+        // 调用
+        User newUser = userService.getUser(user);
+        if (newUser != null) {
+            System.out.println(newUser.getName());
+        } else {
+            System.out.println("user == null");
+        }
+    }
+}
